247b36faec011b5463c1e22a1c4765c6ecdbc578
Sebastian Ramirez <ramirezmatias946@gmail.com>
2021-06-11 12:20:10 -0400

    Cambiamos la configuracion del yargs.

--- src/config/yargs_config.js
+++ src/config/yargs_config.js
@@ -4,6 +4,5 @@ export const yargsArguments = yargs
   .usage("Usage: $0 <command> [options]")
   .command("new", "Inicializa un nuevo proyecto")
   .example("$0 new project1", "Se ha creado un nuevo proyecto")
-  .demandCommand(1)
   .help("h")
   .alias("h", "help").argv;
