1b7e44fa73884f74dd01312ffd3b953139287fb0
Sebastian Ramirez <ramirezmatias946@gmail.com>
2021-07-17 17:51:06 -0400

    yargs

--- src/configs/yargs_config.js
+++ src/configs/yargs_config.js
@@ -1,8 +1,12 @@
 import yargs from "yargs";
 
-export const yargsArguments = yargs
+const yargsArguments = yargs
   .usage("Usage: $0 <command> [options]")
   .command("new", "Initialize a New Project")
   .example("$0 new project1", "Create a New Project")
   .help("h")
   .alias("h", "help").argv;
+
+export {
+  yargsArguments
+}
